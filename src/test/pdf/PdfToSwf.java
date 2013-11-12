a220485b489baf96fdf023986a86ce91358e6cf5
xuyi <xuyi@SAISLT197.sais.com.cn>
2013-11-12 15:51:02 +0800

    123

--- src/test/pdf/PdfToSwf.java
+++ src/test/pdf/PdfToSwf.java
@@ -4,8 +4,8 @@ public class PdfToSwf {
   public static void  main(String[] args){
 	  System.out.println("¿ªÊ¼...");
 	  
-	  String pdf = "D:\\65_2012_EU-2012-cn.pdf";
-	  String swf = "D:\\65_2012_EU-2012-cn.swf";
+	  String pdf = "D:\\E27DF55CA5701A7A5CB8FCA8336A98B97810.pdf";
+	  String swf = "D:\\E27DF55CA5701A7A5CB8FCA8336A98B97810.swf";
 	  pdftoswf(pdf, swf);
 	  System.out.println("½áÊø...");
   }
@@ -20,7 +20,7 @@ public class PdfToSwf {
 									+ pdf
 									+ " "
 									+ swf
-									+ "  -f -T 9 -t -s zoom=120 -s bitmap -s languagedir=C:\\xpdf\\xpdf-chinese-simplified");
+									+ "  -f -T 9 -t -s zoom=200 -s bitmap -s languagedir=C:\\xpdf\\xpdf-chinese-simplified");
 			System.out.println("C:\\SWFTools\\pdf2swf.exe "
 					+ pdf
 					+ " "
